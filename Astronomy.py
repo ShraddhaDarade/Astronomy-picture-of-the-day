80eaca9e584fcc56d3fe84059933ae14c66ebf7f
ShraddhaDarade <shraddhadarade135@gmail.com>
2024-12-31 01:57:56 +0530

    Initial commit with Streamlit app for APOD

---
+++ Astronomy.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import requests
+
+
+api_key="n4KPJuh53F09RgqbmMtqWexNQBoL7bU04qBozgJl"
+url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+
+
+request=requests.get(url)
+
+data=request.json()
+
+title=data["title"]
+image_url=data["url"]
+explaination=data["explanation"]
+
+image_filepath="img.png"
+request2=requests.get(image_url)
+
+with open(image_filepath,"wb") as file:
+    file.write(request2.content)
+
+st.title(title)
+st.image(image_filepath)
+st.write(explaination)
